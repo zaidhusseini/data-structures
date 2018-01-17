8d09f130b7e4d259319f8cd77e4485532b2427c4
David Seidenberg <davidseidenberg@gmail.com>
2018-01-17 12:15:46 -0800

    Implement functional pattern for queue fully (last time made error commit)
    All tests pass.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,12 +8,29 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count ++;
+    storage[count] = value;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+      var result = storage[1];
+      delete storage[1];
+      count --;
+      for (var key in storage) {
+        var value = storage[key];
+        key--;
+        storage[key] = value;
+      }
+      return result;
+    } else {
+      return count;
+    }
+    
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
