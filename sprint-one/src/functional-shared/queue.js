6476ed75e7f0a811ce1f4adf83f912f83804a2a3
David Seidenberg <davidseidenberg@gmail.com>
2018-01-17 15:44:48 -0800

    Implement Queue constructor in functional-shared style

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance['storage'] = {};
+  someInstance['count'] = 0;
+
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    if (this.count === 0) {
+      return undefined;
+    } else {
+      this.count = this.count - 1;
+      var dequeued = this.storage[1];
+      delete this.storage[1];
+      for (var key in this.storage) {
+        this.storage[key - 1] = this.storage[key];
+      }
+      return dequeued;
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
