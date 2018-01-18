b98dc7b93ad28db158c44944f550f9dfe496abd7
zaid <zaidhusseini@gmail.com>
2018-01-17 20:01:46 -0800

    Completed ES6 Instantiation Pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,22 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+    this.index = 0;
   }
 
+  enqueue(value) {
+    this[this.length] = value;
+    this.length++;
+  }
+
+  dequeue(){
+    var temp = this[this.index];
+    this.index++;
+    return temp;
+  }
+
+  size(){
+    return Math.max(0,this.length-this.index);
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
   }
 
+  push(value){
+    this[this.length] = value;
+    this.length++;
+  }
+
+  pop(){
+    this.length--;
+    var temp = this[this.length];
+    this.length = Math.max(0,this.length);
+    return temp;
+  }
+
+  size(){
+    return this.length;
+  }
 }
