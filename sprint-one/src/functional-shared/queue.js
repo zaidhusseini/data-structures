40fa6e4ceb44d925442539c4612872f15f9ed5d7
zaid <zaidhusseini@gmail.com>
2018-01-17 16:05:58 -0800

    Completed Functional Shared Stacks and Queues Implementation

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+var obj = {};
+obj.index = 0;
+obj.length = 0;
+obj.storage = {};
+
+_.extend(obj,queueMethods);
+
+return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+	this[this.length] = value;
+	this.length++;
 
+};
+
+queueMethods.dequeue = function(){
+ var temp = this[this.index];
+ this.index++;
+ return temp;
+	
+};
+
+queueMethods.size = function(){
+  return Math.max(this.length-this.index,0);
+	
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  someInstance.length = 0;
+  someInstance.storage = {};
+
+  _.extend(someInstance,stackMethods);
+  return someInstance;
+
 };
 
 var stackMethods = {};
 
 
+stackMethods.push = function(value){
+	this.storage[this.length] = value;
+	this.length++;
+};
+
+stackMethods.pop = function(){
+	this.length--;
+  var temp = this.storage[this.length];
+  this.length = Math.max(0,this.length);
+  return temp;
+};
+
+stackMethods.size = function(){
+  return this.length;
+};
