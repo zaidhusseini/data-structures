781600c5dcc0c1df6e501dca3219a9e27374c3d6
zaidhusseini <zaidhusseini@gmail.com>
2018-01-18 11:15:45 -0800

    Implement Linked Lists with more robust test cases (e.g., size property, non number primitives)

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,37 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.size = 0;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    list.size++;
   };
 
   list.removeHead = function() {
+    var tempHead = list.head;
+    list.head = list.head.next;
+    list.size--;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    } 
+    return false;  
   };
 
   return list;
@@ -26,4 +49,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+addToTail: O(1)
+removeHead: O(1)
+contains: O(n)
+Node Instantiation: O(1)
  */
