23b2fb914aac3717635d41dc9988f53bd0675d73
zaid <zaidhusseini@gmail.com>
2018-01-17 19:35:50 -0800

    Implemented Pseudoclassical Instantiation Pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.index = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this[this.length] = value;
+  this.length++;
+
+};
+
+Queue.prototype.dequeue = function(){
+  var temp = this[this.index];
+  this.index++;
+  return temp;
+};
+
+Queue.prototype.size = function(){
+  return Math.max(0,this.length-this.index);
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,20 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+
 };
 
+Stack.prototype.push = function(value){
+  this[this.length]=value;
+  this.length++;
+}
 
+Stack.prototype.pop = function(){
+  this.length--;
+  var temp = this[this.length];
+  return temp;
+}
+Stack.prototype.size = function(){
+  return Math.max(this.length,0);
+}
