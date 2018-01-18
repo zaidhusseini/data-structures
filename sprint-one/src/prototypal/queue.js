e8dc87a1081a7b61c953e77b7d93327c08870030
zaid <zaidhusseini@gmail.com>
2018-01-17 18:12:26 -0800

    Added Prototypal pattern for stacks and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(queueMethods);
+  obj.length = 0;
+  obj.index = 0;
+  obj. storage = {};
+
+  return obj;
+
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+  this[this.length] = value;
+  this.length++;
+};
+
+queueMethods.dequeue = function(){
+  var temp = this[this.index];
+  this.index++;
+  return temp;
+
+};
+  
+queueMethods.size = function(){
+  return Math.max(0,this.length-this.index);
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var obj = Object.create(stackMethods);
+  obj.length = 0;
+  obj.storage = {};
+
+  return obj;
+
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.length] = value;
+  this.length++;
 
+};
+
+stackMethods.pop = function(){
+  this.length--;
+  var temp = this.storage[this.length];
+  return temp;
+};
+
+stackMethods.size = function(){
+  return Math.max(0,this.length);
+};
