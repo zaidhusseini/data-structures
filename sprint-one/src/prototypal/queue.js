fe0e9db3bfe112a32675253454bead95b6b36cf6
David Seidenberg <davidseidenberg@gmail.com>
2018-01-17 16:14:08 -0800

    Implement queue constructor with prototypal pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(queueMethods);
+  someInstance['storage'] = {};
+  someInstance['count'] = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  dequeue: function() {
+    if (this.count === 0) {
+      return undefined;
+    }
+    var dequeued = this.storage[1];
+    this.count--;
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    return dequeued;
+  },
+  size: function() {
+    return this.count;
+  }
+};
 
 
